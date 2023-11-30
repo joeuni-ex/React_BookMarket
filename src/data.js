d6128ff006b3e7d8f5e0a3ff476328ee0e4112d6
joeuni <dmsdl8917@gmail.com>
2023-11-30 23:02:28 +0900

    data에 메인 배너 데이터 추가

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 //네브바 링크 섹션
 export const mainLinks = [
-  { id: 1, href: "#home", text: "home" },
-  { id: 2, href: "#about", text: "best" },
-  { id: 3, href: "#services", text: "services" },
-  { id: 4, href: "#tours", text: "tours" },
+  { id: 1, href: "#home", text: "베스트셀러" },
+  { id: 2, href: "#about", text: "이달의이벤트" },
+  { id: 3, href: "#services", text: "추천작" },
+  { id: 4, href: "#tours", text: "신작" },
 ];
