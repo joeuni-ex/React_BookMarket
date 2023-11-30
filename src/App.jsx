dbe17506e45cba53adcb1d15666b4bdd832bb030
joeuni <dmsdl8917@gmail.com>
2023-11-30 23:02:41 +0900

    메인 배너 데이터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import BookList from "./components/BookList";
 import MainBookList from "./components/MainBookList";
 import MainSub from "./components/MainSub";
+import { mainLinks } from "./data";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <div>
         <Navbar />
         <main>
-          <div className="event_cate">
+          {/* <div className="event_cate">
             <ul>
               <li>
                 <a href="">베스트 셀러</a>
@@ -19,13 +20,22 @@ function App() {
             </ul>
           </div>
           <div className="main_event ">
+       
             <MainBookList type="Bestseller" />
-          </div>
-
-          <div className="main_sub_container">
+          </div> */}
+          <div className="main_banner">
             <MainSub />
-            <MainSub />
+            <div className="main_banner_list">
+              <ul>
+                {mainLinks.map((link) => (
+                  <li>
+                    <a href="">{link.text}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
+          {/* <div className="main_sub_container"></div> */}
           <h3 className="main_title">신간 전체 리스트</h3>
           <div className="bookList">
             <BookList type="itemNewAll" />
