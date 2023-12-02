aad8c8fac41057e493b67d0ca8d9aaa0c5735f2b
joeuni <dmsdl8917@gmail.com>
2023-12-02 19:04:18 +0900

    배너 이미지 변경 및 data.js에 import

--- src/data.js
+++ src/data.js
@@ -1,7 +1,12 @@
-//네브바 링크 섹션
-export const mainLinks = [
-  { id: 1, href: "#home", text: "베스트셀러" },
-  { id: 2, href: "#about", text: "이달의이벤트" },
-  { id: 3, href: "#services", text: "추천작" },
-  { id: 4, href: "#tours", text: "신작" },
+import banner1 from "./assets/banner1.jpg";
+import banner2 from "./assets/banner2.jpg";
+import banner3 from "./assets/banner3.jpg";
+import banner4 from "./assets/banner4.jpg";
+
+//메인 배너
+export const mainBanners = [
+  { id: 1, href: "#home", text: "베스트셀러", src: banner1 },
+  { id: 2, href: "#about", text: "이달의이벤트", src: banner2 },
+  { id: 3, href: "#services", text: "추천작", src: banner3 },
+  { id: 4, href: "#tours", text: "신작", src: banner4 },
 ];
