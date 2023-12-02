cf48fde524df9d0453ea378b34d6205dd8489a15
joeuni <dmsdl8917@gmail.com>
2023-12-02 19:06:28 +0900

    안쓰는 컴포넌트 정리 및 Searchbar 컴포넌트 MainBanner컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import "./App.css";
 
 import Navbar from "./components/Navbar/Navbar";
 import BookList from "./components/BookList";
-import MainBookList from "./components/MainBookList";
-import MainSub from "./components/MainSub";
-import { mainLinks } from "./data";
-import Tesx from "./components/Tesx";
+
+import MainBanner from "./components/MainBanner";
+import Searchbar from "./components/Searchbar";
 
 function App() {
   return (
@@ -13,36 +12,16 @@ function App() {
       <div>
         <Navbar />
         <main>
-          {/* <div className="event_cate">
-            <ul>
-              <li>
-                <a href="">베스트 셀러</a>
-              </li>
-            </ul>
-          </div>
-          <div className="main_event ">
-       
-            <MainBookList type="Bestseller" />
-          </div> */}
+          <Searchbar />
           {/* 메인 배너 */}
           <div className="main_banner">
             <div className="main_banner_text">
               <p style={{ color: "black" }}>Book</p>
               <p>Store</p>
             </div>
-
-            <MainSub />
-            <div className="main_banner_list">
-              <ul>
-                {mainLinks.map((link) => (
-                  <li key={link.id}>
-                    <a href="">{link.text}</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <MainBanner />
           </div>
-          {/* <div className="main_sub_container"></div> */}
+
           <h3 className="main_title">신간 전체 리스트</h3>
           <div className="bookList">
             <BookList type="itemNewAll" />
@@ -50,9 +29,6 @@ function App() {
           <h3 className="main_title">주목할 만한 신간 리스트</h3>
           <div className="bookList">
             <BookList type="ItemNewSpecial" />
-          </div>
-          <div style={{ width: "70%" }}>
-            <Tesx />
           </div>
         </main>
         <footer>
