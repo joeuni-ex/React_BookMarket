930588f1334a0de9a7a9d55cc5f139848755afc9
joeuni <dmsdl8917@gmail.com>
2023-12-02 19:05:37 +0900

    BannerItem 컴포넌트 생성

---
+++ src/components/BannerItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./BannerItem.css";
+const BannerItem = ({ src }) => {
+  return (
+    <div className="bannerContent">
+      <a href="/">
+        <p className="bannerText">오늘의 독서</p>
+        <div className="bannerImgContent">
+          <img className="bannerImg" src="" alt="" />
+        </div>
+        <img className="sliderImg" src={src} alt="" />
+      </a>
+    </div>
+  );
+};
+
+export default BannerItem;
