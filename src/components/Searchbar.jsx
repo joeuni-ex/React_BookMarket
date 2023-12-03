caa87b81310885f95f2aeaffb7fee1d9ba5001a2
joeuni <dmsdl8917@gmail.com>
2023-12-03 20:59:05 +0900

    Searchbar css수정 및 검색어 입력 후 검색버튼 클릭 시 Search페이지로 이동

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,21 +1,46 @@
-import React from "react";
-import "./SearchBar.css";
+import React, { useState } from "react";
+import "./Searchbar.css";
 import { FaSearch } from "react-icons/fa";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Searchbar = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  //submit 시
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchValue === "") {
+      alert("내용을 입력해 주세요!");
+    } else {
+      setSearchValue(e.target.value);
+      setSearchValue("");
+      navigate(`/search?q=${searchValue}`); //q= 검색어로 요청한다.
+    }
+  };
+
+  //검색어 입력 시
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    console.log(searchValue);
+  };
+
   return (
     <>
       <div className="searchSection">
-        <form>
-          <label htmlFor="">
+        <form onSubmit={handleSubmit}>
+          <label>
             <input
+              onChange={handleChange}
+              value={searchValue}
               type="text"
               className="search"
               placeholder="검색어를 입력하세요."
             />
-            <a href="" className="searchBtn">
+            <button type="submit" className="searchBtn">
               <FaSearch />
-            </a>
+            </button>
           </label>
         </form>
       </div>
