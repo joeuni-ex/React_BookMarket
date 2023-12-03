d7b987156283b9919ed234d2f7387343d8622dac
joeuni <dmsdl8917@gmail.com>
2023-12-03 19:28:31 +0900

    Navbar 라우터 적용

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,25 @@
 import React from "react";
 import "./Navbar.css";
 import { LuBookOpen } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav_header">
-        <a href="">
+        <Link to="/">
           <LuBookOpen />
           Book
           <span className="nav_market">Store</span>
-        </a>
+        </Link>
       </div>
       <div className="nav_list">
         <ul>
           <li>
-            <a href="">로그인</a>
+            <Link to="/join">회원가입</Link>
           </li>
           <li>
-            <a href="">로그인</a>
-          </li>
-          <li>
-            <a href="">로그인</a>
-          </li>
-          <li>
-            <a href="">로그인</a>
+            <Link to="/login">로그인</Link>
           </li>
         </ul>
       </div>
