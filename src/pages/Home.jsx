5d7e3b1ab4a788fc59aadfb0004d8f867640d213
joeuni <dmsdl8917@gmail.com>
2023-12-03 19:28:47 +0900

    각 페이지 생성

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import MainBanner from "../components/MainBanner";
+import BookList from "../components/BookList";
+
+const Home = () => {
+  return (
+    <>
+      {/* 메인 배너 */}
+      <div className="main_banner">
+        <div className="main_banner_text">
+          <p style={{ color: "black" }}>Book</p>
+          <p>Store</p>
+        </div>
+        <MainBanner />
+      </div>
+      <h3 className="main_title">신간 전체 리스트</h3>
+      <div className="bookList">
+        <BookList type="itemNewAll" />
+      </div>
+      <h3 className="main_title">주목할 만한 신간 리스트</h3>
+      <div className="bookList">
+        <BookList type="ItemNewSpecial" />
+      </div>
+    </>
+  );
+};
+
+export default Home;
