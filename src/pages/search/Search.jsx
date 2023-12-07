25d7efd3a4fa445ccf58488534d49526dbd41b19
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-07 16:22:34 +0900

    정렬 기능 추가

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -11,20 +11,28 @@ const Search = () => {
   const queryParams = new URLSearchParams(queryString);
   const query = queryParams.get("q"); // q에 해당하는 값을 쿼리스트링에서 가져옴
   const [resultBooks, setResultBooks] = useState([]);
+  const [sort, setSort] = useState("Accuracy"); //정렬
 
+  //검색어 변경 시 재 실행
   useEffect(() => {
     fetchBooks();
-  }, [query]);
+  }, [query, sort]);
 
   const fetchBooks = async () => {
     const response = await fetch(
       `http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=${
         import.meta.env.VITE_BOOK_API
-      }&Query=${query}&QueryType=title&MaxResults=10&start=1&SearchTarget=Book&Cover=Big&output=js&Version=20131101`
+      }&Query=${query}&QueryType=title&MaxResults=20&start=1&SearchTarget=Book&Cover=Big&Sort=${sort}&output=js&Version=20131101`
     );
     const data = await response.json();
     setResultBooks(data.item);
     console.log(resultBooks);
+  };
+
+  //정렬하기
+  const handleSortChange = (e) => {
+    setSort(e.target.value);
+    console.log(sort);
   };
 
   return (
@@ -32,6 +40,54 @@ const Search = () => {
       <p style={{ marginBottom: "30px" }}>
         "<span style={{ fontWeight: "bold" }}>{query}</span>"의 검색 결과
       </p>
+      <div className="sort">
+        <form>
+          <p>정렬하기</p>
+          <label>
+            <input
+              onClick={handleSortChange}
+              type="radio"
+              name="sort"
+              value="Accuracy"
+            />
+            정확도순
+          </label>
+          <label>
+            <input
+              onClick={handleSortChange}
+              type="radio"
+              name="sort"
+              value="PublishTime"
+            />
+            출간일순
+          </label>
+          <label>
+            <input
+              onClick={handleSortChange}
+              type="radio"
+              name="sort"
+              value="SalesPoint"
+            />
+            판매량순
+          </label>
+          <label>
+            <input
+              onClick={handleSortChange}
+              type="radio"
+              name="sort"
+              value="CustomerRating"
+            />
+            고객평점순
+          </label>
+        </form>
+
+        {/* <select value={sort} onChange={handleSortChange}>
+          <option value="Accuracy">정확도순</option>
+          <option value="PublishTime ">출간일순</option>
+          <option value="SalesPoint ">판매량순</option>
+          <option value="CustomerRating">고객평점순</option>
+        </select> */}
+      </div>
       <div className="searchBookList">
         {resultBooks.map((book) => (
           <SeachBookCard key={book.itemId} book={book} />
