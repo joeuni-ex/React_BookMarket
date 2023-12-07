85a0b434a830fea48662b59347726d602987ac78
joeuni <dmsdl8917@gmail.com>
2023-12-07 21:54:04 +0900

    로그인/회원가입 폼 생성 및 CSS적용

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { LuBookOpen } from "react-icons/lu";
+import "./Login.css";
 
 const Join = () => {
-  return <div>Join</div>;
+  return (
+    <div className="formContainer">
+      <div className="logo">
+        <LuBookOpen />
+        Book
+        <span className="nav_market">Store</span>
+      </div>
+      <h1>Login</h1>
+      <form>
+        <input type="email" placeholder="이메일을 입력하세요..." />
+        <input type="password" placeholder="비밀번호를 입력하세요..." />
+        <input className="loginBtn" type="submit" value="로그인" />
+      </form>
+    </div>
+  );
 };
 
 export default Join;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./Login.css";
+import { LuBookOpen } from "react-icons/lu";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="formContainer">
+      <div className="logo">
+        <LuBookOpen />
+        Book
+        <span className="nav_market">Store</span>
+      </div>
+      <h1>Login</h1>
+      <form>
+        <input type="email" placeholder="이메일을 입력하세요..." />
+        <input type="password" placeholder="비밀번호를 입력하세요..." />
+        <input className="loginBtn" type="submit" value="로그인" />
+      </form>
+      <div className="googleLogin">
+        <FcGoogle />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
