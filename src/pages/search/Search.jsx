d9f7a6a34dc23049e84fdc515140d9f0857e6c9d
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-07 16:05:14 +0900

    Search 페이지에 컴포넌트 추가 및 css추가

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import BookCard from "../../components/BookCard";
-import "../../components/BookList.css";
+// import "../../components/BookList.css";
+import "./Search.css";
+import SeachBookCard from "./SearchBookCard";
 
 const Search = () => {
   //검색어로 넘어오는 파라미터 가져오기
@@ -12,23 +14,29 @@ const Search = () => {
 
   useEffect(() => {
     fetchBooks();
-  }, []);
+  }, [query]);
 
   const fetchBooks = async () => {
     const response = await fetch(
       `http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=${
         import.meta.env.VITE_BOOK_API
-      }&Query=${query}&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&Cover=Big&output=js&Version=20131101`
+      }&Query=${query}&QueryType=title&MaxResults=10&start=1&SearchTarget=Book&Cover=Big&output=js&Version=20131101`
     );
     const data = await response.json();
     setResultBooks(data.item);
+    console.log(resultBooks);
   };
 
   return (
     <>
-      {resultBooks.map((book) => (
-        <BookCard key={book.itemId} book={book} />
-      ))}
+      <p style={{ marginBottom: "30px" }}>
+        "<span style={{ fontWeight: "bold" }}>{query}</span>"의 검색 결과
+      </p>
+      <div className="searchBookList">
+        {resultBooks.map((book) => (
+          <SeachBookCard key={book.itemId} book={book} />
+        ))}
+      </div>
     </>
   );
 };
