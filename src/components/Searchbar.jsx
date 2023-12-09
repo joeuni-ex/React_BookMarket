dfd81a6d8966e4bca4c2a110c1b24b2895f66b4e
joeuni <dmsdl8917@gmail.com>
2023-12-09 21:36:10 +0900

    실시간 검색 디테일 출력 추가

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -7,6 +7,9 @@ const Searchbar = () => {
   const [searchValue, setSearchValue] = useState("");
   const [resultBooks, setResultBooks] = useState([]); //검색 결과
   const [showResults, setShowResults] = useState(false); // 결과 보이기 여부
+  const [bookDetails, setBookDetails] = useState({}); //검색 상세보기
+  const [onFocus, setOnFocus] = useState(null); // 포커스 상태
+
   const navigate = useNavigate();
 
   const fetchBooks = async () => {
@@ -17,7 +20,18 @@ const Searchbar = () => {
     );
     const data = await response.json();
     setResultBooks(data.item);
-    console.log(resultBooks);
+    // console.log(resultBooks);
+  };
+
+  //실시간 검색 상세보기
+  const fetchBookDetails = async (itemId) => {
+    const response = await fetch(
+      `http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey=${
+        import.meta.env.VITE_BOOK_API
+      }&output=js&Version=20131101&itemIdType=ISBN13&ItemId=${itemId}&Cover=MidBig&OptResult=ebookList,usedList,reviewList`
+    );
+    const data = await response.json();
+    setBookDetails({ ...bookDetails, [itemId]: data.item[0] });
   };
 
   //검색어 실시간 검색기능
@@ -25,15 +39,20 @@ const Searchbar = () => {
     if (searchValue.trim() !== "") {
       fetchBooks();
       setShowResults(true);
+      setOnFocus(null);
     } else {
       setShowResults(false);
     }
   }, [searchValue]);
 
+  useEffect(() => {}, [bookDetails]);
+
   //만약 검색창에 focus가 잡히지 않으면 사라짐
-  const handleFocus = () => {
+  const handleFocus = (itemId) => {
     if (searchValue.trim() !== "") {
       setShowResults(true);
+      fetchBookDetails(itemId);
+      setOnFocus(itemId);
     }
   };
 
@@ -81,13 +100,85 @@ const Searchbar = () => {
         </form>
         {showResults && resultBooks && (
           <div className="resultBooks">
-            <ul>
-              {resultBooks.map((book) => (
-                <a href={book.link} key={book.id}>
-                  <li>{book.title}</li>
-                </a>
-              ))}
-            </ul>
+            <div className="resultTitle">
+              <ul>
+                {resultBooks.map((book) => (
+                  <a key={book.itemId} href={book.link}>
+                    <li onMouseEnter={() => handleFocus(book.isbn13)}>
+                      {book.title.trim().slice(0, 30) + "..."}
+                    </li>
+                  </a>
+                ))}
+              </ul>
+            </div>
+            <div className="resultDetail">
+              {bookDetails[onFocus] && (
+                <>
+                  <div className="resultDetailHead">
+                    <div className="resultDetailCover">
+                      <img src={bookDetails[onFocus].cover} alt="" />
+                    </div>
+                    <div>
+                      <p
+                        style={{
+                          fontSize: "0.9rem",
+                          fontWeight: "bold",
+                          margin: "5px",
+                        }}
+                      >
+                        {bookDetails[onFocus].title}
+                      </p>
+                      <p
+                        style={{
+                          fontSize: "0.8rem",
+                          margin: "5px",
+                        }}
+                      >
+                        {bookDetails[onFocus].publisher}
+                      </p>
+                      <div style={{ display: "flex" }}>
+                        <span
+                          style={{
+                            fontSize: "0.8rem",
+                            color: "green",
+                            fontWeight: "bold",
+                            margin: "5px",
+                          }}
+                        >
+                          10%
+                        </span>
+                        <p
+                          style={{
+                            fontWeight: "bold",
+                            fontSize: "0.8rem",
+                            margin: "5px",
+                          }}
+                        >
+                          {bookDetails[onFocus].priceSales}원
+                        </p>
+                        <p
+                          style={{
+                            textDecoration: "line-through",
+                            lineHeight: "18px",
+                            letterSpacing: "-0.01em",
+                            fontWeight: "lighter",
+                            fontSize: "0.8rem",
+                            margin: "5px",
+                          }}
+                        >
+                          {bookDetails[onFocus].priceStandard}원
+                        </p>
+                      </div>
+                    </div>
+
+                    {/* {console.log(bookDetails[onFocus].item[0])} */}
+                  </div>
+                  <div className="resultDetailBody">
+                    <p>{bookDetails[onFocus].description}</p>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         )}
       </div>
