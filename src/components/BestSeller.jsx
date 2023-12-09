8afe7de272d7ff1071e4a929d901362a9a9c0085
joeuni <dmsdl8917@gmail.com>
2023-12-09 23:24:59 +0900

    베스트셀러 컴포넌트 생성

---
+++ src/components/BestSeller.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./BookList.css";
+import BookCard from "./BookCard";
+
+const BestSeller = ({ type }) => {
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await fetch(
+      `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${
+        import.meta.env.VITE_BOOK_API
+      }&QueryType=${type}&MaxResults=10&start=1&SearchTarget=Book&Cover=Big&output=js&Version=20131101`
+    );
+    const data = await response.json();
+    setBooks(data.item);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, [type]);
+
+  let count = 1;
+  return (
+    <>
+      {books.map((book) => (
+        <div key={book.itemId}>
+          <p style={{ fontWeight: "bold" }}>{count++}</p>
+          <BookCard book={book} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default BestSeller;
