9c39dc584e7cc9eeeb9c60a7ce3e50b3bd1df01e
joeuni <dmsdl8917@gmail.com>
2023-12-11 23:10:52 +0900

    에러 표시 함수 사용

--- src/pages/user/Join.jsx
+++ src/pages/user/Join.jsx
@@ -5,6 +5,7 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../../firebase";
 import { FirebaseError } from "firebase/app";
+import { errorMessageToKorean } from "./auth-components";
 
 const Join = () => {
   const [isLoading, setLoading] = useState(false); //로딩
@@ -50,28 +51,6 @@ const Join = () => {
       if (e instanceof FirebaseError) setError(e.code);
     } finally {
       setLoading(false);
-    }
-  };
-
-  //에러 표시
-  const errorMessageToKorean = (error) => {
-    switch (error) {
-      case "auth/invalid-login-credentials":
-      case "auth/invalid-credential":
-      case "auth/user-not-found" || "auth/wrong-password":
-        return "이메일 혹은 비밀번호가 일치하지 않습니다.";
-      case "auth/email-already-in-use":
-        return "이미 사용 중인 이메일입니다.";
-      case "auth/weak-password":
-        return "비밀번호는 6글자 이상이어야 합니다.";
-      case "auth/network-request-failed":
-        return "네트워크 연결에 실패 하였습니다.";
-      case "auth/invalid-email":
-        return "잘못된 이메일 형식입니다.";
-      case "auth/internal-error":
-        return "잘못된 요청입니다.";
-      default:
-        return "에러가 발생했습니다.";
     }
   };
 
