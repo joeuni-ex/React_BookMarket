4e93bd80eb9361244126ce5fcc7e9a3a118df9bc
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-12 11:10:28 +0900

    유저 로그인 여부에 따른 메뉴 노출  오류 수정

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,13 +7,31 @@ import { auth } from "../../firebase";
 const Navbar = () => {
   const navigate = useNavigate();
   const user = auth.currentUser; //현재 접속 유저
+  const [loggedIn, setLoggedIn] = useState(!!auth.currentUser);
+
+  useEffect(() => {
+    // !!user 존재여부확인 방법
+    // 유저의 인증 상태의 변경 사항을 모니터링하고, 변경되면
+    // 매개변수와 함께 제공된 콜백이 트리거된다.
+    // 그런 다음 상태 업데이트함
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setLoggedIn(!!user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   //console.log(user);
   //로그아웃 함수
   const logOut = () => {
-    auth.signOut();
-    navigate("/");
+    auth.signOut().then(() => {
+      setLoggedIn(false);
+      navigate("/user/login");
+    });
   };
 
+  console.log(user);
   return (
     <>
       <nav className="navbar">
@@ -26,8 +44,13 @@ const Navbar = () => {
         </div>
         <div className="nav_list">
           <ul>
-            {/* 현재 로그인 중인 유저가 아니면 */}
-            {!user && (
+            {/* 로그인 여부에 따라 메뉴 상이 */}
+            {user ? (
+              <>
+                <li>{user.displayName}님 환영합니다!</li>
+                <li onClick={logOut}>로그아웃</li>
+              </>
+            ) : (
               <>
                 <li>
                   <Link to="/user/join">회원가입</Link>
@@ -35,13 +58,6 @@ const Navbar = () => {
                 <li>
                   <Link to="/user/login">로그인</Link>
                 </li>
-              </>
-            )}
-            {/* 현재 로그인중이면*/}
-            {user && (
-              <>
-                <li>{user.displayName}님 환영합니다!</li>
-                <li onClick={logOut}>로그아웃</li>
               </>
             )}
           </ul>
