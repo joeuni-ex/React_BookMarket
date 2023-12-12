81032cee5479ff14e003940c510bee62094142ce
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-12 15:43:13 +0900

    관심 도서 추가 기능 작업중

--- src/pages/search/SearchBookCard.jsx
+++ src/pages/search/SearchBookCard.jsx
@@ -4,12 +4,67 @@ import "./Search.css";
 import { IoIosHeartEmpty } from "react-icons/io";
 import { IoMdHeart } from "react-icons/io";
 import { FaStar } from "react-icons/fa";
+import { auth, db } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  deleteField,
+  updateDoc,
+} from "firebase/firestore";
 
 const SeachBookCard = ({ book }) => {
   const [onHeart, setOnHeart] = useState(false); //하트 클릭 시 색상 변경
+  const [interestBook, setinterestBook] = useState(""); //관심도서 isbn
+  const [isLoading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  const handleClick = () => {
-    setOnHeart(!onHeart);
+  //관심 추가
+  const handleClick = async () => {
+    if (!onHeart) {
+      if (confirm("관심 도서에 추가하겠습니까?")) {
+        const user = auth.currentUser;
+        if (!user) {
+          alert("로그인이 필요한 서비스입니다.");
+          navigate("/user/login");
+        } else {
+          setOnHeart(true);
+          setinterestBook(book.isbn);
+          //console.log(book.isbn);
+        }
+        console.log(interestBook);
+        if (isLoading || interestBook === "") return;
+
+        try {
+          setLoading(true); //로딩시작
+
+          //파이어 스토어에 저장
+          await addDoc(collection(db, "interestBooks"), {
+            interestBook, //BOOK ID
+            createdAt: Date.now(), // 생성일자 오늘
+            username: user.displayName, // 유저 이름
+            userId: user.uid, // 유저 아이디
+          });
+        } catch (e) {
+          console.log(e);
+        } finally {
+          setLoading(false);
+        }
+      } else {
+        alert("취소");
+        return;
+      }
+    } else if (onHeart) {
+      if (confirm("관심 도서에서 제거하겠습니까?")) {
+        alert("확인");
+        setOnHeart(false);
+        //데이터 베이스에서 지워야함
+        await deleteDoc(collection(db, "interestBooks", interestBook));
+      } else {
+        return;
+      }
+    }
   };
 
   return (
