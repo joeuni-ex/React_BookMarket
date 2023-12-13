4cefe3e74c729a81c1967b78fa0c3da497d9f771
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-13 15:37:13 +0900

    관심 도서 컴포넌트 적용

--- src/pages/user/UserPage.jsx
+++ src/pages/user/UserPage.jsx
@@ -1,39 +1,20 @@
-import { useEffect, useState } from "react";
-import { auth, db } from "../../firebase";
+import { useState } from "react";
+import { auth } from "../../firebase";
 import "./UserPage.css";
-import { collection, getDocs, query, where } from "firebase/firestore";
+
+// 리액트 아이콘
+import { IoBookmarks } from "react-icons/io5";
+import InterestedBook from "./InterestedBook";
 
 const UserPage = () => {
   const user = auth.currentUser;
   const [selectedMenu, setSelectedMenu] = useState("관심 도서");
-  const [getInterestBook, setGetInterestBook] = useState([]);
 
   //메뉴 선택
   const handleChange = (value) => {
     setSelectedMenu(value);
   };
 
-  useEffect(() => {
-    //관심목록 가져오는 함수
-    const fetchInterestBooks = async () => {
-      if (user) {
-        //유저가 있을 경우
-        const q = query(
-          collection(db, "interestBooks"),
-          where("userId", "==", user.uid) //로그인 한 유저와 동일한 데이터만
-        );
-
-        const snapshot = await getDocs(q); // 데이터를 다 가져올 때 까지 기다림
-
-        const userInterestBooks = snapshot.docs.map((doc) => doc.data());
-        setGetInterestBook(userInterestBooks);
-      }
-    };
-
-    fetchInterestBooks();
-  }, [user]); // 로그인 유저가 변경 될 때마다 실행
-
-  console.log(getInterestBook);
   return (
     <div className="userpageContainer">
       <div className="aside">
@@ -46,20 +27,13 @@ const UserPage = () => {
         </ul>
       </div>
       <div className="mypage">
-        <h2>{selectedMenu}</h2>
+        <h2>
+          {selectedMenu}
+          {""} <IoBookmarks style={{ color: " #527853" }} />
+        </h2>
 
         <div className="mypageContent">
-          {getInterestBook.map((book) => (
-            <>
-              <div className="interestBook" key={book.interestBook}>
-                <a href={book.bookLink}>
-                  <img src={book.bookCover} alt="관심도서" />
-                </a>
-
-                <p>{book.bookTitle}</p>
-              </div>
-            </>
-          ))}
+          <InterestedBook />
         </div>
       </div>
     </div>
