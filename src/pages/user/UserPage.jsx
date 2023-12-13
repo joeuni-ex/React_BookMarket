c97a51d55b67cc064a00b94dc49d509e0a0215cd
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-13 10:22:10 +0900

    Userpage 기본 틀 작업

--- src/pages/user/UserPage.jsx
+++ src/pages/user/UserPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useState } from "react";
+import { auth } from "../../firebase";
+import "./UserPage.css";
 
 const UserPage = () => {
-  return <div>UserPage</div>;
+  const user = auth.currentUser;
+  const [selectedMenu, setSelectedMenu] = useState("관심 도서");
+
+  //메뉴 선택
+  const handleChange = (value) => {
+    setSelectedMenu(value);
+  };
+  return (
+    <div className="userpageContainer">
+      <div className="aside">
+        <h3>마이페이지</h3>
+        <br />
+        <ul>
+          <li onClick={() => handleChange("관심 도서")}>관심 도서</li>
+          <li onClick={() => handleChange("장바구니")}>장바구니</li>
+          <li onClick={() => handleChange("구매하기")}>구매하기</li>
+        </ul>
+      </div>
+      <div className="mypage">
+        <h2>{selectedMenu}</h2>
+        <div className="mypageContent"></div>
+      </div>
+    </div>
+  );
 };
 
 export default UserPage;
