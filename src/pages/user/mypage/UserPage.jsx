c645bc89d4da35e91607ff3a63ecdd2d01f16697
joeuni <dmsdl8917@gmail.com>
2023-12-15 22:11:56 +0900

    결제 완료 페이지에서 쿼리스트링으로 넘어올 때 처리

--- src/pages/user/mypage/UserPage.jsx
+++ src/pages/user/mypage/UserPage.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth } from "../../../firebase";
 import "./UserPage.css";
 
 // 리액트 아이콘
 import InterestedBook from "./InterestedBook";
+import { useLocation } from "react-router-dom";
+import OrderPage from "./OrderPage";
 
 const UserPage = () => {
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("order"); // q에 해당하는 값을 쿼리스트링에서 가져옴
   const user = auth.currentUser;
   const [selectedMenu, setSelectedMenu] = useState("관심 도서");
+
+  //주문 완료에서 넘어올 때
+  useEffect(() => {
+    if (query === "true") {
+      setSelectedMenu("주문 목록");
+    }
+  }, [query]);
 
   //메뉴 선택
   const handleChange = (value) => {
@@ -22,7 +34,7 @@ const UserPage = () => {
         <ul>
           <li onClick={() => handleChange("관심 도서")}>관심 도서</li>
 
-          <li onClick={() => handleChange("주문/결제")}>주문/결제</li>
+          <li onClick={() => handleChange("주문 목록")}>주문/결제 목록</li>
         </ul>
       </div>
       <div className="mypage">
@@ -31,6 +43,7 @@ const UserPage = () => {
         <div className="mypageContent">
           {/* 관심 도서 컴포넌트 */}
           {selectedMenu === "관심 도서" ? <InterestedBook /> : ""}
+          {selectedMenu === "주문 목록" ? <OrderPage /> : ""}
         </div>
       </div>
     </div>
