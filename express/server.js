cb8004b649cd3877746a09a9e2e94045ba1faad4
joeuni <dmsdl8917@gmail.com>
2023-12-20 22:52:22 +0900

    도서 검색 / 실시간 검색 상세보기 추가

--- express/server.js
+++ express/server.js
@@ -33,6 +33,52 @@ app.get("/ItemList.aspx", async (req, res) => {
   }
 });
 
+// 도서 검색
+app.get("/ItemSearch.aspx", async (req, res) => {
+  //쿼리 값 받기
+  const { Query } = req.query;
+  const aladinApiUrl = `https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=${process.env.BOOK_API_KEY}&Query=${Query}&QueryType=title&MaxResults=7&start=1&SearchTarget=Book&Cover=Big&output=js&Version=20131101`;
+
+  try {
+    // 알라딘 서버에 검색 요청
+    const response = await axios.get(aladinApiUrl);
+    const data = response.data;
+    const result = data.item;
+    // 응답을 받으면 클라이언트에게 전달
+    if (result) {
+      res.status(200).json(result); // Sending 'result' as JSON response
+    } else {
+      res.status(500).send("Internal Server Error");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// 실시간 검색 상세보기
+app.get("/ItemLookUp.aspx", async (req, res) => {
+  //쿼리 값 받기
+  const { itemId } = req.query;
+  const aladinApiUrl = `http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey=${process.env.BOOK_API_KEY}&output=js&Version=20131101&itemIdType=ISBN&ItemId=${itemId}&Cover=MidBig&OptResult=ebookList,usedList,reviewList,ratingInfo`;
+
+  try {
+    // 알라딘 서버에 검색 요청
+    const response = await axios.get(aladinApiUrl);
+    const data = response.data;
+    const result = data.item;
+    // 응답을 받으면 클라이언트에게 전달
+    if (result) {
+      res.status(200).json(result); // Sending 'result' as JSON response
+    } else {
+      res.status(500).send("Internal Server Error");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT} 에서 서버 실행중`);
 });
