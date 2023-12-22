c69274d298dea46e1ea321da4bf6e22c6bebd8a2
joeuni <dmsdl8917@gmail.com>
2023-12-22 21:42:28 +0900

    baseURL수정

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 //axios에 미리 백엔드 앞부분 주소를 저장한다.
 export default axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://react-bookstore-joeuni-ex.netlify.app",
 });
