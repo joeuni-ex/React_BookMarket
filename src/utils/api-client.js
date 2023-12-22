b6dca2be16dfc84e65e538dd5b87ee0c5dcc264f
joeuni <dmsdl8917@gmail.com>
2023-12-22 22:18:12 +0900

    백엔드 url추가

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 //axios에 미리 백엔드 앞부분 주소를 저장한다.
 export default axios.create({
-  baseURL: "https://react-bookstore-joeuni-ex.netlify.app",
+  baseURL: "https://bookstore-backend-ni9g.onrender.com",
 });
