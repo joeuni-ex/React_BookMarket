d4c618ad29ed460b0b5811acbb5b55deb960a903
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-26 16:25:22 +0900

    로딩스켈레톤 테스트

--- src/components/book/BestSeller.jsx
+++ src/components/book/BestSeller.jsx
@@ -1,14 +1,21 @@
 import "./BookList.css";
 import BookCard from "./BookCard";
 import useData from "../../Hook/useData";
+import BookCardSkeleton from "./BookCardSkeleton";
+import Spinner from "../../pages/layout/Spinner";
 
 const BestSeller = ({ type }) => {
-  const { data: bestSeller, error } = useData(`/ItemList?queryType=${type}`);
+  const {
+    data: bestSeller,
+    error,
+    isLoading,
+  } = useData(`/ItemList?queryType=${type}`);
 
   let count = 1;
   return (
     <>
       {error && <em>{error}</em>}
+      {isLoading ? <p>로딩중</p> : ""}
       {bestSeller.map((book) => (
         <div className="bestSeller" key={book.itemId}>
           <div style={{ display: "flex", height: "100%" }}>
---
+++ src/components/book/BookCardSkeleton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+
+const BookCardSkeleton = () => {
+  return <Skeleton className="book" width="180px" />;
+};
+
+export default BookCardSkeleton;
