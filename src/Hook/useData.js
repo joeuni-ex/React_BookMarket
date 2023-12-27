e96599b14ababb8da76cd0b9461c4a6abe0ac284
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-27 12:17:06 +0900

    로딩 초기값 true로 변경

--- src/Hook/useData.js
+++ src/Hook/useData.js
@@ -4,10 +4,11 @@ import apiClient from "../utils/api-client";
 const useData = (endpoint, customConfig, deps) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); // 초기값을 true로 변경
 
   useEffect(
     () => {
+      setIsLoading(true); // 데이터 요청 전에 로딩 상태로 설정
       apiClient
         .get(endpoint, customConfig)
         .then((res) => {
