543475699d7a5960dc33d82247d6a58bccf68199
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-27 12:16:58 +0900

    로딩 중 표시할 스켈레톤 width삭제

--- src/components/book/BookCardSkeleton.jsx
+++ src/components/book/BookCardSkeleton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Skeleton from "react-loading-skeleton";
 
 const BookCardSkeleton = () => {
-  return <Skeleton className="book" width="180px" />;
+  return <Skeleton className="book" />;
 };
 
 export default BookCardSkeleton;
