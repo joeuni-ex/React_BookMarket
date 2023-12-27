90f20be74f53bbf84c3b5ab6f2bda6d626d46264
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-27 12:18:16 +0900

    로딩 중 추가

--- src/components/book/BookList.jsx
+++ src/components/book/BookList.jsx
@@ -1,13 +1,20 @@
 import "./BookList.css";
 import BookCard from "./BookCard";
 import useData from "../../Hook/useData";
+import BookCardSkeleton from "./BookCardSkeleton";
 
 const BookList = ({ type }) => {
-  const { data: books, error } = useData(`/ItemList?queryType=${type}`);
+  const {
+    data: books,
+    error,
+    isLoading,
+  } = useData(`/ItemList?queryType=${type}`);
 
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //로딩
   return (
     <>
       {error && <em>{error}</em>}
+      {isLoading && skeletons.map((n) => <BookCardSkeleton />)}
       {books.map((book) => (
         <BookCard key={book.itemId} book={book} />
       ))}
